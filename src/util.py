c7b4234695c33daeec0af8bfc425e283ee89617c
Eric Melz <eric@emelz.com>
2025-04-07 15:20:09 -0700

    modified format of weight endpoint

--- src/util.py
+++ src/util.py
@@ -6,11 +6,12 @@ def load_weight_data(session_id):
     resp = requests.get("http://localhost/weight", params={"session_id": session_id})
     if resp.status_code == 200:
         data = resp.json()
-        df = pd.DataFrame(data, columns=['Timestamp', 'Weight (lb)'])
+        df = pd.DataFrame(data)
 
-        df['Date'] = pd.to_datetime(df['Timestamp'], unit='s')
+        df['Date'] = pd.to_datetime(df['timestamp'], unit='s')
         df.set_index('Date', inplace=True)
-        df.drop(columns='Timestamp', inplace=True)
+        df.drop(columns='timestamp', inplace=True)
+        df.rename(columns={'weight_lbs': 'Weight (lb)'}, inplace=True)
         df['Year'] = df.index.year
         df['Day of Year'] = df.index.dayofyear
         return df
